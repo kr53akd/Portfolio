f1a90259ce2b350f7ef4e9b9f9f18cc8a5dc675c
kr53akd <76629016+kr53akd@users.noreply.github.com>
2022-09-09 20:45:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul li');
 const header = document.querySelector('.header ,.container');
 
 hamburger.addEventListener('click',()=>{
